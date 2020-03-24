1465afb4778da238dbc1d7b2ec6a88a7b75c88ca
Feng Tien <feng.tien@gmail.com>
2020-03-24 12:47:49 -0700

    Updates

---
+++ precache-manifest.268fcc494ccc94b5b3b9422a56e342d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae580964f485d13d3646422d07044363",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7e37dcdfaa16dde67b3a.bundle.js"
+  },
+  {
+    "url": "runtime~main.7e37dcdfaa16dde67b3a.bundle.js"
+  },
+  {
+    "revision": "2e918e5110b94785f195",
+    "url": "static/css/main.31866dd2.chunk.css"
+  },
+  {
+    "url": "vendors~main.7e37dcdfaa16dde67b3a.bundle.js"
+  },
+  {
+    "url": "vendors~main.7e37dcdfaa16dde67b3a.bundle.js.LICENSE.txt"
+  }
+]);
